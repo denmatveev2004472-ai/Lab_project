31efa174a46a323d171e02c22e9b2a86df7120e4
denmatveev2004472-ai <denmatveev2004472@gmail.com>
2026-08-04 19:41:49 +0300

    fix: autopush path with cyrillic folder name

--- autopush.ps1
+++ autopush.ps1
@@ -1,4 +1,4 @@
-$folder = "C:\Users\denma\OneDrive\\u0420\u0430\u0431\u043e\u0447\u0438\u0439 \u0441\u0442\u043e\u043b\Lab_project"
+$folder = $PSScriptRoot
 
 $watcher = New-Object System.IO.FileSystemWatcher
 $watcher.Path = $folder
@@ -7,7 +7,7 @@ $watcher.EnableRaisingEvents = $true
 
 $action = {
     Start-Sleep -Seconds 3
-    Set-Location $folder
+    Set-Location $Event.MessageData
     $status = git status --porcelain
     if ($status) {
         git add -A
@@ -18,10 +18,10 @@ $action = {
     }
 }
 
-Register-ObjectEvent $watcher "Changed" -Action $action
-Register-ObjectEvent $watcher "Created" -Action $action
-Register-ObjectEvent $watcher "Deleted" -Action $action
-Register-ObjectEvent $watcher "Renamed" -Action $action
+Register-ObjectEvent $watcher "Changed" -Action $action -MessageData $folder
+Register-ObjectEvent $watcher "Created" -Action $action -MessageData $folder
+Register-ObjectEvent $watcher "Deleted" -Action $action -MessageData $folder
+Register-ObjectEvent $watcher "Renamed" -Action $action -MessageData $folder
 
 Write-Host "Watching: $folder"
 while ($true) { Start-Sleep -Seconds 1 }
